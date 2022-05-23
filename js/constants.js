4b6000e593bd35c75d2591c2d4d412d93c10ffdf
manuelcastano <manuelsabe186@gmail.com>
2022-05-23 17:45:37 -0500

    .

--- js/constants.js
+++
@@ -1,4 +0,0 @@
-const BASE_URL = "http://6.tcp.ngrok.io:16584/lunchticket";
-const AUTH_URL = "https://pi2sis.icesi.edu.co/saamfiapi/public/institutions/1/systems/4/users/login";
-
-export {BASE_URL, AUTH_URL};
