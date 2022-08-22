6a26822882aef8a331215f6474a84fab73ea8c5c
Mateo Loaiza <thetraceurs@gmail.com>
2022-08-22 18:39:21 -0500

    POST from web app mostly done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,93 @@
-import logo from './applogo.png';
-import React from 'react';
-import './App.css';
+import logo from "./applogo.png";
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({ data: [] });
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState("");
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onUser = (event) => {
+    setUsername(event.target.value);
+  };
+  /* Arrow function que hace esto mismo
+  function onUser(event) {
+    setUsername(event.target.value);
+  }
+  */
+  const onPass = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleClick = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(
+        "https://www.icesi.edu.co/uccareapi/auth/institutions/1/authentication/login",
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password,
+          }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+
+      console.log("result is: ", result);
+
+      //Destructuring del objeto. Saco estos atributos
+      const { persName, persLastname, persIddocument } = result;
+
+      //Creo un objeto con dichos atributos
+      const out = {
+        persName,
+        persLastname,
+        persIddocument,
+      };
+
+      //Falta hacer un POST al BACKEND
+      
+      setData(result);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="Login">
-      <img id="logo" src={logo}/>
+      <img src={logo} />
       <p>Cédula</p>
-      <input id="inputCedula" type="text" placeholder="Número de cédula"/>
+      <input onChange={onUser} type="text" placeholder="Número de cédula" />
       <p>Contraseña de banner</p>
-      <input id="inputContrasena" type="password" placeholder="Contraseña"/>
-      <br/>
-      <button id="loginButton">Iniciar sesión</button>
-      <br/>
-      <pr id="badLogin" >Credenciales incorrectas</pr>
+      <input onChange={onPass} type="password" placeholder="Contraseña" />
+      {isLoading && <p>Cargando</p>}
+      {err && (
+        <p class="text-danger text-center fw-bold">
+          Usuario o Contraseña incorrectos
+        </p>
+      )}
+      <br />
+      <button class="btn btn-primary" onClick={handleClick}>
+        Iniciar sesión
+      </button>
+      <br />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     //<App />,
-    <Dashboard />,
+    <App />,
     document.getElementById('root')
 );
