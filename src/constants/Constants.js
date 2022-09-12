6abacf8ff449e30a8085b0d255281cd31a333474
manuelcastano <manuelsabe186@gmail.com>
2022-09-12 09:52:34 -0500

    Fixed Manuels VS to run front and changed couple of things in login

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,2 +1,2 @@
-export const BASEURL = "https://c174-186-27-157-17.ngrok.io"
+export const BASEURL = "https://fd47-200-3-193-226.ngrok.io"
 export const AUTHURL = "https://www.icesi.edu.co/uccareapi/auth/institutions/1/authentication/login"
