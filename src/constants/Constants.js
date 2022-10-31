f814c17dedb993bdaf933ed1b6fed736a72b8028
Andres Mayor <andresmayor03@gmail.com>
2022-10-31 15:20:02 -0500

    Variable de entorno

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,2 +1,2 @@
-export const BASEURL = "http://localhost:8081/lunchticket"
+export const BASEURL =  process.env.BACK_ADDR ||"https://localhost:8081/lunchticket"
 export const AUTHURL = "https://www.icesi.edu.co/uccareapi/auth/institutions/1/authentication/login"
