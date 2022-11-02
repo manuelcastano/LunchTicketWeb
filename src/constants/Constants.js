fb55b72909da81805b960d050975052bcd479c94
ML7 <thetraceurs@gmail.com>
2022-11-02 18:51:00 -0500

    Changed option in BaseURL

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,4 +1,4 @@
 
-export const BASEURL =  process.env.BACK_ADDR ||"http://localhost:8081/lunchticket"
+export const BASEURL =  process.env.BACK_ADDR ||"https://i2thub.icesi.edu.co:5443/lunchticket"
 
 export const AUTHURL = "https://www.icesi.edu.co/uccareapi/auth/institutions/1/authentication/login"
